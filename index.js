3a42af65b2f7c48fa2265f7d2eace1e857d2984d
yasuhiro otsubo <yasuhirootsubo@M1.local>
2021-09-12 13:11:06 +0900

    09/12/13:11

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-<script type="text/javascript">
-  $(function(){
+$(function(){
     //注文リストにインデックス追加
     $(".order-list-group-item").each(function(index){
-      //console.log(index);
-      $(this).find("ul li:first-child").text("注文" + (index + 1));
+        //console.log(index);
+        $(this).find("ul li:first-child").text("注文" + (index + 1));
     });    
 
     //送信
     $('form').submit(function(){
-      var date = "2021/10/10";
-      var category = $("#category option:selected").val();
-      var amount = $("#amount option:selected").val();
-          
-      var msg = `納品日：${date}\n種類：${category}\n個数：${amount}`;
-      send_text(msg);
-      
-      return false;
+        var date = "2021/10/10";
+        var category = $("#category option:selected").val();
+        var amount = $("#amount option:selected").val();
+            
+        var msg = `納品日：${date}\n種類：${category}\n個数：${amount}`;
+        send_text(msg);
+        
+        return false;
     });
-  });
-</script>
+});
