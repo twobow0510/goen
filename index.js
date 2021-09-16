acaa98595bb3facb4ad1397924aa3bfc412382d9
yasuhiro otsubo <yasuhirootsubo@M1.local>
2021-09-16 12:10:18 +0900

    modify at 09/16/12:10

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ $(function(){
             $(".order-list-group-item").each(function(index) {
                 $(this).find("p:first-child").text("注文" + (index + 1));
 
-                $(this).find("p.category").attr({"id": `category${index+1}`});
+                $(this).find("p.category").attr({"class": `category${index+1}`});
                 $(this).find("p.category label").attr({"for": `category${index+1}`});
                 $(this).find("p.category select").attr({"name": `category${index+1}`});
 
-                $(this).find("p.amount").attr({"id": `amount${index+1}`});
+                $(this).find("p.amount").attr({"class": `amount${index+1}`});
                 $(this).find("p.amount label").attr({"for": `amount${index+1}`});
                 $(this).find("p.amount select").attr({"name": `amount${index+1}`});
             });
